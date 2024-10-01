978824f870c34a1f8ae8835c7a16468928f0cc64
Felipe Sao Paulo <felipeasaopaulo@gmail.com>
2024-10-01 16:13:47 -0300

    Filling in form and asserting if did correctly

--- tests/automationExercise.spec.ts
+++ tests/automationExercise.spec.ts
@@ -9,9 +9,20 @@ test.beforeEach('Navigate to the page', async ({ page }) => {
 });
 
 test('Sign Up Testing', async ({ page })  => {
+
+  // Navigate to the signup page and add credentials
   await page.getByText('Signup / Login').click();
   await page.waitForURL('https://automationexercise.com/login')
-  await page.getByPlaceholder('Name').fill('Test Name')
-  await page.locator('.signup-form').getByPlaceholder('Email Address').fill('test@address.io')
+  await page.getByPlaceholder('Name').fill('Malenia, Blade of Miquella')
+  await page.locator('.signup-form').getByPlaceholder('Email Address').fill('malenia@eldenring.boss')
   await page.getByRole('button', {name: 'Signup'}).click()
+  expect(page.url()).toEqual('https://automationexercise.com/signup')
+
+  // Fill in account information form
+  await expect(page.getByLabel('Name *', { exact: true })).toHaveValue('Malenia, Blade of Miquella')
+  await expect(page.getByLabel('Email *', { exact: true })).toHaveValue('malenia@eldenring.boss')
+  await page.getByRole('radio', {name: 'Mr.'}).check()
+  await page.getByLabel('Password *', { exact: true }).fill('strong-passWorD1')
+
+
 })
