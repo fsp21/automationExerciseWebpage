155bc32423ccebf71a37141703a8e523710dcefa
Felipe Sao Paulo <felipeasaopaulo@gmail.com>
2024-10-01 18:43:32 -0300

    Added login and delete flow, small variable refactor

--- tests/automationExercise.spec.ts
+++ tests/automationExercise.spec.ts
@@ -1,19 +1,22 @@
 import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
-test.beforeEach('Navigate to the page', async ({ page }) => {
-  await page.goto('https://automationexercise.com');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Automation Exercise');
-});
-
-test('Sign Up Testing', async ({ page })  => {
+test.describe('Sign Up and Login flow', async () => {
 
   const email = faker.internet.email()
   const firstName = faker.person.firstName()
   const lastName = faker.person.lastName()
   const fullName = firstName + ' ' + lastName
+  const password = faker.internet.password()
+
+  test.beforeEach('Navigate to the page', async ({ page }) => {
+    await page.goto('https://automationexercise.com');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle('Automation Exercise');
+  });
+  
+test('Signup Flow', async ({ page })  => {
 
   // Navigate to the signup page and add credentials
   await page.getByText('Signup / Login').click();
@@ -27,7 +30,7 @@ test('Sign Up Testing', async ({ page })  => {
   await expect(page.getByLabel('Name *', { exact: true })).toHaveValue(fullName)
   await expect(page.getByLabel('Email *', { exact: true })).toHaveValue(email)
   await page.getByRole('radio', {name: 'Mr.'}).check()
-  await page.getByLabel('Password *', { exact: true }).fill(faker.internet.password())
+  await page.getByLabel('Password *', { exact: true }).fill(password)
   await page.locator('#days').selectOption('1')
   await page.locator('#months').selectOption('February')
   await page.locator('#years').selectOption('1900')
@@ -43,4 +46,34 @@ test('Sign Up Testing', async ({ page })  => {
   await page.getByText('Mobile Number').fill(String(faker.number.bigInt()))
   await page.getByRole('button', {name: 'Create Account'}).click()
   expect(page.url()).toBe('https://automationexercise.com/account_created')
+})
+
+test('Login Flow', async ({ page }) => {
+
+  await page.getByText('Signup / Login').click();
+  await page.waitForURL('https://automationexercise.com/login')
+  await page.locator('.login-form').getByPlaceholder('Email Address').fill(email)
+  await page.getByPlaceholder('Password').fill(password)
+  await page.getByRole('button', {name: 'Login'}).click()
+  await expect(page.getByText('Logout')).toBeVisible();
+
+})
+
+test('Delete Account Flow', async ({ page }) => {
+
+  // Login 
+  await page.getByText('Signup / Login').click();
+  await page.waitForURL('https://automationexercise.com/login')
+  await page.locator('.login-form').getByPlaceholder('Email Address').fill(email)
+  await page.getByPlaceholder('Password').fill(password)
+  await page.getByRole('button', {name: 'Login'}).click()
+  await expect(page.getByText('Logout')).toBeVisible();
+
+  // Delete account
+  await expect(page.getByText('Delete Account')).toBeVisible();
+  await page.getByText('Delete Account').click();
+  await expect(page.getByText('Your account has been permanently deleted!')).toBeVisible();
+
+})
+
 })
