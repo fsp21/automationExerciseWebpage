4c359e836b0d10fd51b6693c4463d00b04cdc333
Felipe Sao Paulo <felipeasaopaulo@gmail.com>
2024-10-01 15:36:16 -0300

    Working on signup scriptW

--- tests/automationExercise.spec.ts
+++ tests/automationExercise.spec.ts
@@ -11,4 +11,7 @@ test.beforeEach('Navigate to the page', async ({ page }) => {
 test('Sign Up Testing', async ({ page })  => {
   await page.getByText('Signup / Login').click();
   await page.waitForURL('https://automationexercise.com/login')
+  await page.getByPlaceholder('Name').fill('Test Name')
+  await page.locator('.signup-form').getByPlaceholder('Email Address').fill('test@address.io')
+  await page.getByRole('button', {name: 'Signup'}).click()
 })
